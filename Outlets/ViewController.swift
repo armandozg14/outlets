93ea434121a12fe9fc8a31818f9b5ea1af2f8aee
analourdescr <ana_lourdes98@hotmail.com>
2022-08-16 09:39:39 -0700

    Outlets

--- Outlets/ViewController.swift
+++ Outlets/ViewController.swift
@@ -9,9 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Un controller es una conexion entre un Control de UI del Storyboard y una
+    //variable de Codigo Swift
+    @IBOutlet weak var lblMiEtiqueta: UILabel!
+    
+    //Esta funcion se ejecuta ya que cargo todo en memoria
+    //Lo que pongamos aqui sobre escribe lo del storyboard
     override func viewDidLoad() {
         super.viewDidLoad()
+        lblMiEtiqueta.text = "Este es texto desde codigo"
+        lblMiEtiqueta.textColor = UIColor.blue
         // Do any additional setup after loading the view.
     }
 
